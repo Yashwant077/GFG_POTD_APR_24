bf5bd6620bcc472f66fc9e2825971f0ffa9ae43f
Yashwant Choudhary <84570757+Yashwant077@users.noreply.github.com>
2024-04-11 11:48:19 +0530

    Create 11-04-24-Gray_to_Binary_equivalent.cpp

---
+++ 11-04-24-Gray_to_Binary_equivalent.cpp
@@ -0,0 +1,89 @@
+/*
+Problem Description: Gray to Binary equivalent
+
+Given an integer number n, which is a decimal representation of Gray Code. Find the binary equivalent of the Gray Code & return the decimal representation of the binary equivalent.
+
+ 
+
+Note: Please visit here to learn How Gray Code is generated.
+
+Example 1:
+
+Input: 
+n = 4
+Output: 
+7
+Explanation:
+Given 4, its gray code =  110.
+Binary equivalent of the gray code 110 is 100.
+Return 7 representing gray code 100.
+
+
+Example 2:
+
+Input: 
+n = 15
+Output: 
+10
+Explanation:
+Given 15 representing gray code 1000.
+Binary equivalent of gray code 1000 is 1111.
+Return 10 representing gray code 1111 
+ie binary 1010.
+Your Task: 
+You don't need to read input or print anything. Your task is to complete the function grayToBinary() which accepts an integer n as an input parameter and returns decimal representation of the binary equivalent of the given gray code.
+
+Expected Time Complexity: O(log (n)).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+0 <= n <= 10^8
+
+*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    int grayToBinary(int n)
+    {
+        // Your code here
+        int ans=n;
+        n=n/2;
+        
+        while(n>0){
+            ans^=n;
+            n=n/2;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t,n;
+    cin>>t;//testcases
+    while(t--)
+    {
+        cin>>n;//initializing n
+        
+        Solution ob;
+        //calling function grayToBinary()
+       cout<< ob.grayToBinary(n)<<endl;
+    }
+}
+
+// } Driver Code Ends
