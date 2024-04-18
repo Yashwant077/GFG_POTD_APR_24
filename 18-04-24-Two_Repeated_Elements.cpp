d8fade6266a668b80cf35ab416cda6f6c9fe7421
Yashwant Choudhary <84570757+Yashwant077@users.noreply.github.com>
2024-04-18 17:16:39 +0530

    Create 18-04-24-Two_Repeated_Elements.cpp

---
+++ 18-04-24-Two_Repeated_Elements.cpp
@@ -0,0 +1,89 @@
+/*
+Problem Description: Two Repeated Elements
+
+You are given an integer n and an integer array arr of size n+2. All elements of the array are in the range from 1 to n. Also, all elements occur once except two numbers which occur twice. Find the two repeating numbers.
+Note: Return the numbers in their order of appearing twice. So, if X and Y are the repeating numbers, and X's second appearance comes before second appearance of Y, then the order should be (X, Y).
+
+Example 1:
+
+Input:
+n = 4
+arr[] = {1,2,1,3,4,3}
+Output: 
+1 3
+Explanation: 
+In the given array, 1 and 3 are repeated two times and as 1's second appearance occurs before 2's second appearance, so the output should be 1 3.
+
+
+Example 2:
+
+Input:
+n = 2
+arr[] = {1,2,2,1}
+Output: 
+2 1
+Explanation: 
+In the given array, 1 and 2 are repeated two times and second occurence of 2 comes before 1. So the output is 2 1.
+Your Task:
+The task is to complete the function repeatedElements() which takes an integer array arr[] and an integer n as inputs (the size of the array is n + 2 and elements are in the range [1, n]) and finds the two repeated elements in the array and return them in an array.
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(1). 
+
+Constraints:
+2 ≤ n ≤ 10^5
+1 ≤ arr[i] ≤ n
+
+
+*/
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    //Function to find two repeated elements.
+    vector<int> twoRepeated (int arr[], int n) {
+        // Your code here
+        vector<int>ans;
+        for(int i=0;i<n+2;i++){
+            int ind=abs(arr[i]);
+            if(arr[ind]>0) arr[ind]=-arr[ind];
+            else ans.push_back(ind);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    
+    while(t--)
+    {
+        cin>>n;
+        
+        int a[n+2];
+        
+        for(int i=0;i<n+2;i++)
+            cin>>a[i];
+        
+        Solution obj;
+        vector<int> res;
+        res = obj.twoRepeated(a, n);
+        cout<<res[0]<<" "<<res[1]<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
